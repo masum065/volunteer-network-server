22daf53ce7218e9e407ba80a3d00dbd72289f295
masum065 <masumbillah065@gmail.com>
2020-10-03 19:13:24 +0600

    Adding vlunteer network server files

---
+++ routes/index.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+
+const fs = require('fs');
+const {
+  promisify
+} = require('util');
+const pipeline = promisify(require('stream').pipeline);
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+  });
+});
+
+const upload = multer();
+const MongoClient = require('mongodb').MongoClient;
+const uri =
+  'mongodb+srv://rider:XBy6BoY1A6WyHZ8e@cluster1.fwfle.mongodb.net/events?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const eventCollections = client.db('events').collection('programs');
+
+  // Add new event From User 
+  router.post('/upload', upload.single('file'), async function (
+    req,
+    res,
+    next
+  ) {
+    const url = req.protocol + '://' + req.get('host');
+    const {
+      file,
+      body: {
+        title,
+        date,
+        description
+      },
+    } = req;
+    const fileName = Date.now() * 110000 + file.detectedFileExtension;
+    await pipeline(
+      file.stream,
+      fs.createWriteStream(`${__dirname}/../public/images/${fileName}`)
+    );
+    res.send(req.file);
+
+    const eventData = {
+      imageBanner: url + '/images/' + fileName,
+      title,
+      description,
+      date,
+    };
+
+    eventCollections.insertOne(eventData).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // show/ Update Operation
+  router.get('/events', (req, res) => {
+    eventCollections.find({}).toArray((errr, docs) => {
+      res.send(docs);
+    });
+  });
+
+
+
+});
+
+router.get('/images', (req, res) => {
+  res.send(`${__dirname}/../public/images`);
+});
+
+module.exports = router;
